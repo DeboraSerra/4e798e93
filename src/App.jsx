5fd423db623e7bd19a8dbb206cf1f5a19344500f
Débora Serra <debora.r.serra@gmail.com>
2024-07-12 20:51:30 -0300

    chore: minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import Header from './Header.jsx';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import ArchivedCalls from "./components/AllCalls/ArchivedCalls.jsx";
+import AllCalls from "./components/AllCalls/index.jsx";
+import Header from "./components/Header/index.jsx";
+import "./css/allCalls.css";
+import "./css/card.css";
 
 const App = () => {
+  const [active, setActive] = useState("all");
+
   return (
-    <div className='container'>
-      <Header/>
-      <div className="container-view">Some activities should be here</div>
-    </div>
+      <div className='container'>
+        <Header active={active} setActive={setActive} />
+        <div className='container-view'>
+          {active === "archive" ? <ArchivedCalls /> : null}
+          {active === "all" ? <AllCalls /> : null}
+        </div>
+      </div>
   );
 };
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/body.css';
 import './css/app.css';
 import './css/header.css';
+import './utils/env.js'
 
-import App from './App.jsx';
+import './App.jsx';
 
